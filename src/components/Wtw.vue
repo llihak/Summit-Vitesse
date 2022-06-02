10d8ac546bc9afbbf296434cb938491c933a59ca
keith hill <keith.hill@metastock.com>
2022-06-02 14:38:16 -0600

    dummy text

--- src/components/Wtw.vue
+++ src/components/Wtw.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <div class="grid grid-cols-2 bg-green-300" p="4">
     <h3>What the what, anyhow ??</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aspernatur pariatur ratione recusandae voluptatum atque libero unde architecto soluta maiores, sunt odio quas laudantium? A amet doloribus excepturi perspiciatis molestiae.</p>
+    <p>A bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text  bunch of dummy text </p>
   </div>
 </template>
 
